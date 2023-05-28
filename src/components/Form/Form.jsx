7200077902a2ad22426b479244bac7cda09508f2
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-05-28 18:04:00 +0300

    Update Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -20,20 +20,14 @@ const schema = yup.object().shape({
     .trim()
     .max(64)
     .required('Name is required')
-    .matches(nameRegex, {
-      message:
-        "Invalid name. Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan.",
-    }),
+    .matches(nameRegex),
 
   number: yup
     .string()
     .trim()
     .required('Number is required')
     .min(5)
-    .matches(numberRegex, {
-      message:
-        'Invalid number. Phone number must be digits and can contain spaces, dashes, parentheses and can start with +.',
-    }),
+    .matches(numberRegex),
 });
 
 export const ContactForm = () => {
