37b8a6ffda66c7f7a5fa708c5bd6ed0bff1fc09e
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-05-28 17:57:25 +0300

    fix

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 import css from './List.module.css';
 // import css from '../buttons/IconBtn/IconBtn.module.css';
 import { ReactComponent as DeleteIcon } from '../../assets/close.svg';
@@ -10,15 +10,6 @@ import { deleteContact } from 'redux/contacts/operations';
 import { selectFilteredContacts } from 'redux/contacts/selectors';
 
 export const ContactList = () => {
-  // const contacts = useSelector(state => state.contacts.items);
-  // const filter = useSelector(state => state.contacts.filter);
-
-  // const isVisibleContacts = () =>
-  //   contacts.filter(contact =>
-  //     contact.name.toLowerCase().includes(filter.toLowerCase())
-  //   );
-  // const filterContact = isVisibleContacts();
-  // const dispatch = useDispatch();
   const filteredContacts = useSelector(selectFilteredContacts);
 
   const dispatch = useDispatch();
@@ -44,4 +35,10 @@ export const ContactList = () => {
       })}
     </ul>
   );
+};
+
+ContactList.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  number: PropTypes.string.isRequired,
 };
