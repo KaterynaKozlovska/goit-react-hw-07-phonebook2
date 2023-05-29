2079e9ef900a04aa5403a9011ce0329f4a13473f
KaterynaKozlovska <kkozlovska30@gmail.com>
2023-05-29 19:51:51 +0300

    Update Message.jsx

--- src/components/Message/Message.jsx
+++ src/components/Message/Message.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { css } from './Message.module.css';
+import css from './Message.module.css';
 
 function Message({ message }) {
   return <>{message && <div className={css.message}>{message}</div>}</>;
